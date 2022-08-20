6d95b8c591edc22bbfee235ead8c54b3c9a7456c
k-wachira <abkelvinwachira@gmail.com>
2022-08-21 00:50:16 +0300

    Eliminate null accounts from payload

--- packages/server/controllers/reviewerController.js
+++ packages/server/controllers/reviewerController.js
@@ -2,7 +2,7 @@ const pool = require("../db");
 
 module.exports.getUnverified = async (req, res) => {
   const pendingProfiles = await pool.query(
-    "SELECT id, first_name, last_name, gender, age, dob, nationality, id_type, id_number, id_url FROM users u WHERE u.is_verified='false'"  );
+    "SELECT id, first_name, last_name, gender, age, dob, nationality, id_type, id_number, id_url FROM users u WHERE u.is_verified='false' AND u.id_number IS NOT NULL "  );
   if (pendingProfiles.rowCount > 0) {
     res.json(pendingProfiles.rows);
   } else {
